4ad9a83ec77b25d00b02b35dc64d195ba3db8ba9
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-02-26 15:21:25 -0300

    Add files via upload

---
+++ data.php
@@ -0,0 +1,28 @@
+<?php
+  echo 'Jogo sorteado: '
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Data</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <?php
+      $us = [
+'GTA', 'Call of dutty', 'FIFA', 'Battlefield', 'PUBG', 'Minecraft', 'Free fire', 'Guitar hero',
+'Fortnite', 'League of legends', 'Dota 2', 'War of warcraft', 'The sims', 'Need for speed', 
+'Roblox', 'Super Mario', 'Bomberman', 'Mortal combat', 'Tekken', 'Driver', 'PES'
+      ];
+
+      $nu1 = rand(0,17);
+      
+      echo $us[rand(0,17)];
+
+      
+  ?>
+</body>
+</html>
